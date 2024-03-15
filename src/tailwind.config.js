ec07fd5d5bb9ab470a0a5682513c1dd6bd2d3a24
Dazank <adriandimoko2015@gmail.com>
2024-03-15 22:23:19 +0100

    first commit

---
+++ src/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["../index.html"],
+  theme: {
+    extend: {
+      colors:{"Off-Black": "hsl(0, 0%, 8%)",
+      "Dark-Grey": "hsl(0, 0%, 12%)",
+      "Ultra-Green": "hsl(75, 94%, 57%)",
+      "Neo-Gray": "hsl(0, 0%, 20%)",
+
+      }
+    },
+  },
+  plugins: [],
+}
+
